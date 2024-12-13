7349e9e22d4d3b8ca7d8de306b84c83633856457
bendyashka <tu.dastan.tu@gmail.com>
2024-12-14 01:38:11 +0600

    добавление картинок персоажам из api

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 // API Star Wars
-const apiUrl = "https://swapi.dev/api/people/";
+const apiUrl = "https://swapi.dev/api/people";
 
 
 const characterList = document.getElementById('characterList');
@@ -39,3 +39,42 @@ searchBar.addEventListener('input', (e) => {
   );
   displayCharacters(filteredCharacters);
 });
+
+const characterImages = {
+  "Luke Skywalker": "https://nsabers.es/cdn/shop/articles/opolar_httpss.mj.runWO-xsj2B4pM_A_super_realistic_portrait_of_d96eeb79-b220-46ac-9305-fe7a83dfaf4f_0.png?v=1722400402",
+  "C-3PO": "https://images.bauerhosting.com/legacy/empire-images/features/560ebc7b50e6c513721c309f/C-3PO.jpg?ar=16%3A9&fit=crop&crop=top&auto=format&w=undefined&q=80",
+  "R2-D2": "https://lumiere-a.akamaihd.net/v1/images/r2-d2-main_f315b094.jpeg?region=431%2C0%2C536%2C536",
+  "Darth Vader": "https://nsabers.es/cdn/shop/articles/celehey_Generate_an_image_of_Darth_Vader_standing_in_a_dark_o_b4b944ee-2afc-425b-b707-e95eb776c395_1.png?v=1710756391",
+  "Leia Organa": "https://swrpggm.com/wp-content/uploads/2020/01/liea2esb.png",
+"Owen Lars": "https://static.wikia.nocookie.net/starwars/images/8/81/Owen-OP.jpg/revision/latest?cb=20070626181249",
+"Beru Whitesun lars": "https://static.wikia.nocookie.net/starwars/images/7/76/Beru_headshot2.jpg/revision/latest?cb=20111029215429",
+"R5-D4": "https://www.looper.com/img/gallery/the-mandalorian-r5-d4-might-just-have-the-series-best-redemption-story/l-intro-1681921066.jpg",
+"Biggs Darklighter": "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQpsRy66tukIrnTNGYyi1iSw2QmvUGEi_KGdw&s",
+"Obi-Wan Kenobi": "https://www.liveabout.com/thmb/F5lfgFptU9DNTDCT-xNEtot0lQ0=/1500x0/filters:no_upscale():max_bytes(150000):strip_icc()/EP2-IA-60435_R_8x10-56a83bea3df78cf7729d314a.jpg",
+
+	
+
+ 
+};
+
+function displayCharacters(characters) {
+  characterList.innerHTML = '';
+  characters.forEach(character => {
+    const characterCard = document.createElement('div');
+    characterCard.classList.add('character-card');
+    const imageUrl = characterImages[character.name] || 'https://via.placeholder.com/200x300?text=No+Image';
+
+    characterCard.innerHTML = `
+      <img src="${imageUrl}" alt="${character.name}">
+      <h3>${character.name}</h3>
+      <p>Height: ${character.height}</p>
+      <p>Mass: ${character.mass}</p>
+      <div class="additional-info">
+        <p>Birth Year: ${character.birth_year}</p>
+        <p>Gender: ${character.gender}</p>
+      </div>
+    `;
+
+    characterList.appendChild(characterCard);
+  });
+}
